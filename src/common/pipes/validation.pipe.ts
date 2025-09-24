ed1b58e67e6e83e384ef35229463062f82c1c344
Anuolu-2020 <Anuolu1645@gmail.com>
2025-09-24 23:36:06 +0100

    feat(logger): add request logger with colored output

---
+++ src/common/pipes/validation.pipe.ts
@@ -0,0 +1,41 @@
+import {
+	ArgumentMetadata,
+	BadRequestException,
+	Injectable,
+	PipeTransform,
+} from "@nestjs/common";
+import * as Joi from "joi";
+
+@Injectable()
+export class JoiValidationPipe implements PipeTransform {
+	constructor(private readonly schema: Joi.ObjectSchema) {}
+
+	async transform(value: any, metadata: ArgumentMetadata) {
+		if (!value) {
+			throw new BadRequestException("Request body is empty");
+		}
+
+		try {
+			if (
+				(value instanceof Object && metadata.type === "body") ||
+				(typeof value === "object" && metadata.type === "body")
+			) {
+				await this.schema.validateAsync(value, {
+					abortEarly: false,
+				});
+
+				return value;
+			}
+
+			return value;
+		} catch (error) {
+			if (error instanceof Joi.ValidationError) {
+				const errorMessages = error.details
+					.map((detail) => detail.message.replace(/"/g, ""))
+					.join(", ");
+				throw new BadRequestException(`${errorMessages}`);
+			}
+			throw error;
+		}
+	}
+}
