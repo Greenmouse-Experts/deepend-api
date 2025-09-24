322526fc106b85b645121ef07fcf756b1ee2f753
Anuolu-2020 <Anuolu1645@gmail.com>
2025-09-24 23:36:25 +0100

    feat: add password service for hashing and comparing passwords

---
+++ src/core/password/password.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import * as argon2 from "argon2";
+
+@Injectable()
+export class PasswordService {
+	async hashPassword(rawPassword: string) {
+		return await argon2.hash(rawPassword);
+	}
+
+	async comparePassword(
+		password: string,
+		hashedPassword: string,
+	): Promise<boolean> {
+		const isMatch = await argon2.verify(hashedPassword, password);
+
+		return isMatch;
+	}
+}
