30a66f828f65cc4d8ee2e91a31ec96276194bf75
Anuolu-2020 <Anuolu1645@gmail.com>
2025-09-24 23:37:25 +0100

    feat: add env schema validation with joi

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app/app.controller";
+import { AppService } from "./app/app.service";
+import { ConfigModule } from "@nestjs/config";
+import { envSchema } from "./app.envSchema";
+import { DatabaseModule } from "./database/database.module";
+import { CoreModule } from './core/core.module';
+
+@Module({
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			validationSchema: envSchema,
+		}),
+    DatabaseModule,
+    CoreModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
+})
+export class AppModule {}
