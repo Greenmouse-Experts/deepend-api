f9661736c62f9686e06ec0256b0659c240dae793
Anuolu-2020 <Anuolu1645@gmail.com>
2025-09-30 01:18:45 +0100

    feat(core): add admin module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { Module } from "@nestjs/common";
+import { UserModule } from "./user/user.module";
+import { AuthModule } from "./auth/auth.module";
+import { AdminModule } from "./admin/admin.module";
 
 @Module({
-  imports: [UserModule, AuthModule]
+	imports: [UserModule, AuthModule, AdminModule],
 })
 export class CoreModule {}
