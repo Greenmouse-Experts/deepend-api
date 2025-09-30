72e28c983da76f7517cc613cfa6ca65b4ea6cc95
Anuolu-2020 <Anuolu1645@gmail.com>
2025-09-30 02:03:24 +0100

    feat(admin): add API property to category DTOs

--- src/core/admin/dto/category.dto.ts
+++ src/core/admin/dto/category.dto.ts
@@ -1,7 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
 import * as Joi from "joi";
 
 export class CreateFoodCategoryDto {
+	@ApiProperty({
+		example: "Fruits",
+		description: "The name of the food category",
+	})
 	name: string;
+
+	@ApiProperty({
+		example: "A category for all kinds of fruits",
+		description: "The description of the food category",
+		required: false,
+	})
 	description?: string;
 }
 
@@ -11,7 +22,17 @@ export const CreateFoodCategorySchema = Joi.object({
 });
 
 export class UpdateFoodCategoryDto {
+	@ApiProperty({
+		example: "Fruits",
+		description: "The name of the food category",
+	})
 	name: string;
+
+	@ApiProperty({
+		example: "A category for all kinds of fruits",
+		description: "The description of the food category",
+		required: false,
+	})
 	description?: string;
 }
 
