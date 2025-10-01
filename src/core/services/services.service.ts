042c45dde6aa3529d45d906d78881fe406e0f7d8
Anuolu-2020 <Anuolu1645@gmail.com>
2025-10-01 02:18:39 +0100

    feat: add services service
    This service handles food categories and food retrieval.

---
+++ src/core/services/services.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from "@nestjs/common";
+import { ServicesRepository } from "./service.repository";
+
+@Injectable()
+export class ServicesService {
+	constructor(private readonly servicesRepository: ServicesRepository) {}
+
+	async getFoodCategories(page: number, limit: number) {
+		const offset = (page - 1) * limit;
+		return await this.servicesRepository.getFoodCategories(offset, limit);
+	}
+
+	async getFoodById(id: string) {
+		return await this.servicesRepository.getFoodById(id);
+	}
+
+	async getAllFoods({
+		page,
+		limit,
+		categoryId,
+		search,
+	}: {
+		page: number;
+		limit: number;
+		categoryId?: number;
+		search?: string;
+	}) {
+		const offset = (Number(page) - 1) * (Number(limit) + 1);
+
+		const foods = await this.servicesRepository.getAllFoods({
+			offset,
+			limit,
+			categoryId,
+			search,
+		});
+
+		if (foods.length > limit) {
+			foods.pop();
+			return {
+				foods,
+				pagination: {
+					page,
+					limit,
+					nextPage: Number(page) + 1,
+					prevPage: Number(page) - 1 > 0 ? Number(page) - 1 : null,
+				},
+			};
+		}
+
+		return {
+			foods,
+			pagination: {
+				page,
+				limit,
+				nextPage: null,
+				prevPage: Number(page) - 1 > 0 ? Number(page) - 1 : null,
+			},
+		};
+	}
+}
