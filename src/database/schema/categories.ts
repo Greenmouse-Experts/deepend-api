6f1c1eb676ae5f5a0f73f848a2945b3f60e98af9
Anuolu-2020 <Anuolu1645@gmail.com>
2025-10-01 02:20:09 +0100

    feat: Add food addon categories and items tables
    This commit introduces new tables for managing food addon categories and
    items, allowing for a more structured approach to adding extras to menu
    items. It also establishes relationships between these tables and
    existing food categories.

--- src/database/schema/categories.ts
+++ src/database/schema/categories.ts
@@ -4,12 +4,38 @@ import {
 	timestamp,
 	varchar,
 	mysqlTable,
+	decimal,
 } from "drizzle-orm/mysql-core";
 
 export const foodCategories = mysqlTable("food_categories", {
 	id: int("id").primaryKey().autoincrement(),
 	name: varchar("name", { length: 255 }).notNull().unique(),
 	description: text("description"),
+	icon: text("icon"),
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at")
+		.notNull()
+		.$onUpdateFn(() => new Date()),
+});
+
+export const foodAddonCategories = mysqlTable("food_addon_categories", {
+	id: int("id").primaryKey().autoincrement(),
+	name: varchar("name", { length: 255 }).unique().notNull(),
+	description: text("description"),
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at")
+		.notNull()
+		.$onUpdateFn(() => new Date()),
+});
+
+export const foodAddonsItems = mysqlTable("food_addons_items", {
+	id: int("id").primaryKey().autoincrement(),
+	name: varchar("name", { length: 255 }).unique().notNull(),
+	description: text("description"),
+	price: decimal("price", { precision: 10, scale: 2 }).notNull(),
+	categoryId: int("category_id")
+		.references(() => foodAddonCategories.id)
+		.notNull(),
 	createdAt: timestamp("created_at").defaultNow().notNull(),
 	updatedAt: timestamp("updated_at")
 		.notNull()
