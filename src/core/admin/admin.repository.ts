3f672cc56fedc07888b01cc897d6ddd4beea8756
Anuolu-2020 <Anuolu1645@gmail.com>
2025-10-01 02:18:11 +0100

    feat: Add food management and addon functionalities
    This commit introduces functionality for managing food items and their
    associated addons within the admin panel.
    
    - Implemented endpoints for creating, updating, deleting, and retrieving
    food items.
    - Added support for associating food items with addon categories and
    individual addon items.
    - Enabled removal of addon categories and items from food items.
    - Included retrieval of food items with their addon categories and
    items.
    - Added functionality for creating, updating, deleting, and retrieving
    addon categories and items.
    - Added functionality for making food items available or unavailable.

--- src/core/admin/admin.repository.ts
+++ src/core/admin/admin.repository.ts
@@ -1,25 +1,176 @@
 import { Injectable } from "@nestjs/common";
-import { eq } from "drizzle-orm";
+import { and, eq, inArray, sql } from "drizzle-orm";
 import { DatabaseService } from "src/database/database.service";
-import { foodCategories } from "src/database/schema/categories";
+import {
+	foodAddonCategories,
+	foodAddonsItems,
+	foodCategories,
+} from "src/database/schema/categories";
+import {
+	CreateFood,
+	foods,
+	FoodToAddonsCategories,
+	foodToAddonsCategories,
+	foodToAddonsItems,
+	FoodToAddonsItems,
+} from "src/database/schema/services";
 
 @Injectable()
 export class AdminRepository {
 	constructor(private readonly databaseService: DatabaseService) {}
 
-	async createFoodCategory(name: string, description?: string) {
+	async createFood(foodData: CreateFood) {
+		const result = await this.databaseService.db
+			.insert(foods)
+			.values(foodData)
+			.$returningId();
+
+		return result[0];
+	}
+
+	async updateFood(id: string, foodData: Partial<CreateFood>) {
+		const result = await this.databaseService.db
+			.update(foods)
+			.set(foodData)
+			.where(eq(foods.id, id));
+
+		return result;
+	}
+
+	async deleteFood(id: string) {
+		const result = await this.databaseService.db
+			.delete(foods)
+			.where(eq(foods.id, id));
+
+		return result;
+	}
+
+	async getFoodById(id: string) {
+		const food = await this.databaseService.db
+			.select()
+			.from(foods)
+			.where(eq(foods.id, id));
+
+		return food;
+	}
+
+	async addFoodAddons(
+		addonCategories: FoodToAddonsCategories[],
+		addonItems: FoodToAddonsItems[],
+	) {
+		return await this.databaseService.db.transaction(async (tx) => {
+			if (addonCategories.length > 0) {
+				await tx
+					.insert(foodToAddonsCategories)
+					.values(addonCategories)
+					.onDuplicateKeyUpdate({ set: { foodId: sql`food_id` } });
+			}
+
+			if (addonItems.length > 0) {
+				await tx.insert(foodToAddonsItems).values(addonItems);
+			}
+		});
+	}
+
+	async removeFoodAddonCategory(foodId: string, addonCategoryIds: number[]) {
+		return await this.databaseService.db.transaction(async (tx) => {
+			if (addonCategoryIds.length > 0) {
+				await tx
+					.delete(foodToAddonsItems)
+					.where(
+						and(
+							eq(foodToAddonsItems.foodId, foodId),
+							inArray(foodToAddonsItems.addonCategoryId, addonCategoryIds),
+						),
+					);
+
+				const deletedFoodAddonCategory = await tx
+					.delete(foodToAddonsCategories)
+					.where(
+						and(
+							eq(foodToAddonsCategories.foodId, foodId),
+							inArray(foodToAddonsCategories.addonCategoryId, addonCategoryIds),
+						),
+					);
+
+				return deletedFoodAddonCategory;
+			}
+		});
+	}
+
+	async removeFoodAddonItems(foodId: string, addonItemIds: number[]) {
+		return await this.databaseService.db
+			.delete(foodToAddonsItems)
+			.where(
+				and(
+					eq(foodToAddonsItems.foodId, foodId),
+					inArray(foodToAddonsItems.addonItemId, addonItemIds),
+				),
+			);
+	}
+
+	async getFoodAddonsByFoodId(foodId: string) {
+		const result =
+			await this.databaseService.db.query.foodToAddonsCategories.findMany({
+				where: eq(foodToAddonsCategories.foodId, foodId),
+				with: {
+					category: {
+						with: {
+							items: {
+								columns: {
+									createdAt: false,
+									updatedAt: false,
+								},
+							},
+						},
+					},
+				},
+			});
+
+		return result.map((item) => ({
+			id: item.category.id,
+			name: item.category.name,
+			description: item.category.description,
+			items: item.category.items,
+		}));
+	}
+
+	async getFoodByName(name: string) {
+		const food = await this.databaseService.db
+			.select()
+			.from(foods)
+			.where(eq(foods.name, name));
+
+		return food;
+	}
+
+	async getAllFoods(offset: number, limit: number) {
+		const foodsList = await this.databaseService.db
+			.select()
+			.from(foods)
+			.limit(limit)
+			.offset(offset);
+		return foodsList;
+	}
+
+	async createFoodCategory(name: string, description?: string, icon?: string) {
 		const result = await this.databaseService.db
 			.insert(foodCategories)
-			.values({ name, description })
+			.values({ name, description, icon })
 			.$returningId();
 
 		return result;
 	}
 
-	async updateFoodCategory(id: number, name: string, description?: string) {
+	async updateFoodCategory(
+		id: number,
+		name: string,
+		description?: string,
+		icon?: string,
+	) {
 		const result = await this.databaseService.db
 			.update(foodCategories)
-			.set({ name, description })
+			.set({ name, description, icon })
 			.where(eq(foodCategories.id, id));
 
 		return result;
@@ -57,5 +208,113 @@ export class AdminRepository {
 			.limit(limit)
 			.offset(offset);
 		return categories;
+	}
+
+	async createFoodAddonCategories(
+		addons: { name: string; description?: string }[],
+	) {
+		const result = await this.databaseService.db
+			.insert(foodAddonCategories)
+			.values(addons)
+			.$returningId();
+
+		return result;
+	}
+
+	async updateFoodAddonCategory(
+		id: number,
+		name: string,
+		description?: string,
+	) {
+		const result = await this.databaseService.db
+			.update(foodAddonCategories)
+			.set({ name, description })
+			.where(eq(foodAddonCategories.id, id));
+
+		return result;
+	}
+
+	async deleteFoodAddonCategory(id: number) {
+		const result = await this.databaseService.db
+			.delete(foodAddonCategories)
+			.where(eq(foodAddonCategories.id, id));
+
+		return result;
+	}
+
+	async getAllFoodAddonCategories(offset: number, limit: number) {
+		const categories = await this.databaseService.db
+			.select()
+			.from(foodAddonCategories)
+			.limit(limit)
+			.offset(offset);
+
+		return categories;
+	}
+
+	async createFoodAddonItems(
+		addonItems: {
+			categoryId: number;
+			name: string;
+			description?: string;
+			price: string;
+		}[],
+	) {
+		return await this.databaseService.db
+			.insert(foodAddonsItems)
+			.values(addonItems)
+			.$returningId();
+	}
+
+	async updateFoodAddonItem(
+		id: number,
+		data: {
+			categoryId?: number;
+			name?: string;
+			description?: string;
+			price?: string;
+		},
+	) {
+		return await this.databaseService.db
+			.update(foodAddonsItems)
+			.set(data)
+			.where(eq(foodAddonsItems.id, id));
+	}
+
+	async deleteFoodAddonItem(id: number) {
+		return await this.databaseService.db
+			.delete(foodAddonsItems)
+			.where(eq(foodAddonsItems.id, id));
+	}
+
+	async getAllFoodAddonItemsByCategory(
+		categoryId: number,
+		offset: number,
+		limit: number,
+	) {
+		return await this.databaseService.db
+			.select()
+			.from(foodAddonsItems)
+			.where(eq(foodAddonsItems.categoryId, categoryId))
+			.limit(limit)
+			.offset(offset);
+	}
+
+	async makeFoodAvailable(id: string) {
+		const result = await this.databaseService.db
+			.update(foods)
+			.set({ isAvailable: true })
+			.where(eq(foods.id, id));
+
+		return result;
+	}
+
+	async makeFoodUnavailable(id: string) {
+		const result = await this.databaseService.db
+			.update(foods)
+			.set({ isAvailable: false })
+			.where(eq(foods.id, id));
+
+		return result;
 	}
 }
