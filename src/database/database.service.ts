270d565072b649081374e0cf4c5a3678dc87d1da
Anuolu-2020 <Anuolu1645@gmail.com>
2025-10-06 23:11:16 +0100

    feat(database): initialize and check database connection
    This commit initializes the database connection using Drizzle ORM and
    adds a check to ensure the connection is healthy upon module
    initialization. It also includes logic to gracefully close the
    connection during application shutdown.

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,11 +1,11 @@
 import {
-    Inject,
+	Inject,
 	Injectable,
 	Logger,
 	OnApplicationShutdown,
 	OnModuleInit,
 } from "@nestjs/common";
-import type {Connection} from "mysql2/promise";
+import type { Connection } from "mysql2/promise";
 import { drizzle, type MySql2Database } from "drizzle-orm/mysql2";
 import * as schema from "./schema";
 
@@ -13,30 +13,33 @@ import * as schema from "./schema";
 export class DatabaseService implements OnModuleInit, OnApplicationShutdown {
 	private readonly logger = new Logger(DatabaseService.name);
 
-  public db: MySql2Database<typeof schema>
+	public db: MySql2Database<typeof schema>;
 
-  constructor(@Inject('MYSQL_CONNECTION') private connection: Connection) {
-    this.db = drizzle(this.connection,{  schema: schema, mode: "default" }); 
- }
+	constructor(@Inject("MYSQL_CONNECTION") private connection: Connection) {
+		this.db = drizzle(this.connection, {
+			schema: schema,
+			mode: "default",
+		});
+	}
 
-  async onModuleInit() {
-    this.logger.log("Database module initialized");
-    await this.checkConnection();
-  }
+	async onModuleInit() {
+		this.logger.log("Database module initialized");
+		await this.checkConnection();
+	}
 
-  async checkConnection() {
-    try {
-      await this.connection.ping();
-      this.logger.log("Database connection is healthy");
-    } catch (error) {
-      this.logger.error("Database connection failed", error);
-      throw error;
-    }
-  }
+	async checkConnection() {
+		try {
+			await this.connection.ping();
+			this.logger.log("Database connection is healthy");
+		} catch (error) {
+			this.logger.error("Database connection failed", error);
+			throw error;
+		}
+	}
 
-  async onApplicationShutdown() {
-    this.logger.log("Shutting down database connection");
+	async onApplicationShutdown() {
+		this.logger.log("Shutting down database connection");
 
-    await this.connection.end();
-  }
+		await this.connection.end();
+	}
 }
