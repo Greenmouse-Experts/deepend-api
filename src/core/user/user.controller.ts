320cbaf0aba936c3ff68f458b82c4d31bbe41157
Anuolu-2020 <Anuolu1645@gmail.com>
2025-10-16 16:50:47 +0100

    feat(user): add get user studio bookings endpoint

--- src/core/user/user.controller.ts
+++ src/core/user/user.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from "@nestjs/common";
+import { ApiOperation } from "@nestjs/swagger";
+import { GetUser } from "src/common/decorators/get-user.decorator";
+import { Role } from "src/common/decorators/role/role.decorator";
+import { UserRoles } from "src/common/decorators/role/role.enum";
+import {
+	StudioBookingPaginationQueryDto,
+	StudioBookingPaginationQuerySchema,
+} from "src/common/dto/requestQuery.dto";
+import { QueryJoiValidationPipe } from "src/common/pipes/query-validation.pipe";
+import { UserService } from "./user.service";
+import { AuthGuard } from "src/common/guards/auth.guard";
 
-@Controller('user')
-export class UserController {}
+@Controller({ path: "users", version: "1" })
+@UseGuards(AuthGuard)
+@Role(UserRoles.User)
+export class UserController {
+	constructor(private readonly userService: UserService) {}
+
+	@Get("studio-bookings")
+	@ApiOperation({ summary: "Get user's studio bookings with pagination" })
+	async getUserStudioBookings(
+		@GetUser("id") userId: string,
+		@Query(new QueryJoiValidationPipe(StudioBookingPaginationQuerySchema))
+		{ page, limit, status }: StudioBookingPaginationQueryDto,
+	) {
+		return await this.userService.getUserStudioBookings({
+			userId,
+			page,
+			limit,
+			status,
+		});
+	}
+}
