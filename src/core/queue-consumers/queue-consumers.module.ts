d92c743c8f0cab9908e5b8e984f40f28af67b62f
Anuolu-2020 <Anuolu1645@gmail.com>
2025-11-01 17:33:37 +0100

    feat(queue-consumers): add webhook queue processing with BullMQ
    This change integrates BullMQ for managing webhook queue processing. It
    registers a queue named `WEBHOOKS_QUEUE` and includes it in the module's
    imports. The `BullModule` is also exported to allow other modules to
    utilize it.

--- src/core/queue-consumers/queue-consumers.module.ts
+++ src/core/queue-consumers/queue-consumers.module.ts
@@ -2,9 +2,19 @@ import { Module } from "@nestjs/common";
 import { WebhooksListener } from "./webhooks.listener";
 import { WebhooksConsumer } from "./webhooks.consumer";
 import { PaymentModule } from "../payment/payment.module";
+import { BullModule } from "@nestjs/bullmq";
+import { WEBHOOKS_QUEUE } from "src/common/constants";
+import { UserModule } from "../user/user.module";
 
 @Module({
-	imports: [PaymentModule],
+	imports: [
+		BullModule.registerQueue({
+			name: WEBHOOKS_QUEUE,
+		}),
+		PaymentModule,
+		UserModule,
+	],
 	providers: [WebhooksConsumer, WebhooksListener],
+	exports: [BullModule],
 })
 export class QueueConsumersModule {}
