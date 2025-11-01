69d0ec6705678bfedb055e0eef3e6f3fc6f00267
Anuolu-2020 <Anuolu1645@gmail.com>
2025-11-01 17:34:48 +0100

    feat(user): add snack add-ons to movie order response

--- src/core/webhooks/webhooks.controller.ts
+++ src/core/webhooks/webhooks.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req, Res } from "@nestjs/common";
+import type { Request, Response } from "express";
+import { WebhooksService } from "./webhooks.service";
 
-@Controller('webhooks')
-export class WebhooksController {}
+@Controller({ path: "webhooks", version: "1" })
+export class WebhooksController {
+	constructor(private readonly webhooksService: WebhooksService) {}
+
+	@Post("paystack")
+	async handleWebhook(
+		@Req() request: Request,
+		@Res()
+		response: Response,
+	) {
+		const paystackHeaderSign = request.headers[
+			"x-paystack-signature"
+		] as string;
+
+		if (!paystackHeaderSign) {
+			return response.status(400).json("Missing Paystack signature header");
+		}
+
+		const body = request.body;
+
+		await this.webhooksService.handleWebhookEvent(paystackHeaderSign, body);
+
+		return response.status(200).send();
+	}
+}
