3069641890b97580af401ed2287470537564e392
Anuolu-2020 <Anuolu1645@gmail.com>
2025-11-01 02:14:59 +0100

    feat: add constants for ticket and webhook configurations

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,2 +1,5 @@
 export const MYSQL_CONNECTION = "MYSQL_CONNECTION";
 export const ID_GENERATOR_LENGTH = 15;
+export const TICKET_ID_LENGTH = 10;
+export const SUCCESSFUL_PAYMENT_WEBHOOK = "successful_payment-webhook";
+export const WEBHOOKS_QUEUE = "webhooks";
