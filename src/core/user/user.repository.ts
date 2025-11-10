18efecd94fa2d0a8bbda29dfcb4736df0e2ca483
Anuolu-2020 <Anuolu1645@gmail.com>
2025-11-10 23:05:33 +0100

    feat(user): add user tickets and reciepts functionalities
    This commit introduces functionalities for retrieving user tickets and
    receipts, including details and listing. It expands the UserRepository
    to handle movie, vrgame, and studio tickets, as well as equipment,
    hotel, and food receipts.
    ```

--- src/core/user/user.repository.ts
+++ src/core/user/user.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { and, eq, inArray, sql } from "drizzle-orm";
+import { and, eq, inArray, or, sql } from "drizzle-orm";
 import { DatabaseService } from "src/database/database.service";
 import {
 	// AddToCartType,
@@ -20,6 +20,9 @@ import {
 	hotelCart,
 	equipmentRentalsCart,
 	moviesTicketCart,
+	cinemas,
+	cinemaHalls,
+	hotels,
 } from "src/database/schema";
 import {
 	EquipmentRentalBookingStatus,
@@ -1072,5 +1075,382 @@ export class UserRepository {
 			.update(hotelBookings)
 			.set(updateData)
 			.where(inArray(hotelBookings.id, bookingIds));
+	}
+
+	async getUserTickets({
+		userId,
+		offset,
+		limit,
+	}: { userId: string; limit: number; offset: number }) {
+		const movieTickets = this.databaseService.db
+			.select({
+				ticketId: movieTicketPurchases.ticketId,
+				type: sql<string>`'movie'`,
+				category: movieTicketPurchases.genre,
+				title: movieTicketPurchases.movieName,
+				imageUrl: movieTicketPurchases.movieImageUrl,
+				ageRating: cinemaMovies.ageRating,
+				date: movieTicketPurchases.showDate,
+				time: movieTicketPurchases.showtime,
+				qrcodeData: movieTicketPurchases.qrcodeData,
+			})
+			.from(movieTicketPurchases)
+			.where(
+				and(
+					eq(movieTicketPurchases.userId, userId),
+					or(
+						eq(movieTicketPurchases.status, "completed"),
+						eq(movieTicketPurchases.status, "cancelled"),
+					),
+				),
+			)
+			.leftJoin(
+				cinemaMovies,
+				eq(movieTicketPurchases.movieId, cinemaMovies.id),
+			);
+
+		const vrgameTickets = this.databaseService.db
+			.select({
+				ticketId: vrgameTicketPurchases.ticketId,
+				type: sql<string>`'vrgame'`,
+				category: vrgameTicketPurchases.vrgameCategory,
+				title: vrgameTicketPurchases.vrgameName,
+				imageUrl: vrgameTicketPurchases.vrgameImageUrl,
+				ageRating: vrgames.ageRating,
+				date: vrgameTicketPurchases.scheduledDate,
+				time: vrgameTicketPurchases.scheduledTime,
+				qrcodeData: vrgameTicketPurchases.qrcodeData,
+			})
+			.from(vrgameTicketPurchases)
+			.where(
+				and(
+					eq(vrgameTicketPurchases.userId, userId),
+					or(
+						eq(vrgameTicketPurchases.status, "completed"),
+						eq(vrgameTicketPurchases.status, "cancelled"),
+					),
+				),
+			)
+			.leftJoin(vrgames, eq(vrgameTicketPurchases.vrgameId, vrgames.id));
+
+		const studioTickets = this.databaseService.db
+			.select({
+				ticketId: studioSessionBookings.id,
+				type: sql<string>`'studio'`,
+				category: sql<string>`'Studio Session'`,
+				title: sql<string>`CONCAT('Studio Session at ', ${studioSessionBookings.studioName})`,
+				imageUrl: sql<string>`''`,
+				ageRating: sql<number>`0`,
+				date: studioSessionBookings.sessionDate,
+				time: studioSessionBookings.sessionStartTime,
+				qrcodeData: studioSessionBookings.qrcodeData,
+			})
+			.from(studioSessionBookings)
+			.leftJoin(studios, eq(studioSessionBookings.studioId, studios.id))
+			.where(
+				and(
+					eq(studioSessionBookings.userId, userId),
+					or(
+						eq(studioSessionBookings.status, "completed"),
+						eq(studioSessionBookings.status, "scheduled"),
+						eq(studioSessionBookings.status, "cancelled"),
+					),
+				),
+			);
+
+		const tickets = await unionAll(movieTickets, vrgameTickets, studioTickets)
+			.limit(limit)
+			.offset(offset);
+
+		return tickets;
+	}
+
+	async getUserTicketDetailsById({
+		ticketId,
+		userId,
+		ticketType,
+	}: {
+		ticketId: string;
+		userId: string;
+		ticketType: "movie" | "vrgame" | "studio";
+	}) {
+		if (ticketType === "movie") {
+			return await this.databaseService.db
+				.select({
+					movieTitle: movieTicketPurchases.movieName,
+					movieImageUrl: movieTicketPurchases.movieImageUrl,
+					genre: movieTicketPurchases.genre,
+					showDate: movieTicketPurchases.showDate,
+					showtime: movieTicketPurchases.showtime,
+					price: movieTicketPurchases.ticketPrice,
+					cinemaName: cinemas.name,
+					cinemaHallName: movieTicketPurchases.cinemaHallName,
+					cinemaLocation: movieTicketPurchases.location,
+					location: movieTicketPurchases.location,
+					qrcodeData: movieTicketPurchases.qrcodeData,
+				})
+				.from(movieTicketPurchases)
+				.where(
+					and(
+						eq(movieTicketPurchases.ticketId, ticketId),
+						eq(movieTicketPurchases.userId, userId),
+					),
+				)
+				.leftJoin(
+					cinemaHalls,
+					eq(movieTicketPurchases.cinemaHallId, cinemaHalls.id),
+				)
+				.leftJoin(cinemas, eq(cinemaHalls.cinemaId, cinemas.id));
+		} else if (ticketType === "vrgame") {
+			return await this.databaseService.db
+				.select({
+					vrgameName: vrgameTicketPurchases.vrgameName,
+					vrgameImageUrl: vrgameTicketPurchases.vrgameImageUrl,
+					vrgameCategory: vrgameTicketPurchases.vrgameCategory,
+					scheduledDate: vrgameTicketPurchases.scheduledDate,
+					scheduledTime: vrgameTicketPurchases.scheduledTime,
+					price: vrgameTicketPurchases.ticketPrice,
+					qrcodeData: vrgameTicketPurchases.qrcodeData,
+				})
+				.from(vrgameTicketPurchases)
+				.where(
+					and(
+						eq(vrgameTicketPurchases.ticketId, ticketId),
+						eq(vrgameTicketPurchases.userId, userId),
+					),
+				);
+		} else if (ticketType === "studio") {
+			return await this.databaseService.db
+				.select({
+					studioName: studioSessionBookings.studioName,
+					sessionDate: studioSessionBookings.sessionDate,
+					sessionStartTime: studioSessionBookings.sessionStartTime,
+					sessionEndTime: studioSessionBookings.sessionEndTime,
+					price: studioSessionBookings.totalPrice,
+					qrcodeData: studioSessionBookings.qrcodeData,
+				})
+				.from(studioSessionBookings)
+				.where(
+					and(
+						eq(studioSessionBookings.id, ticketId),
+						eq(studioSessionBookings.userId, userId),
+					),
+				);
+		} else {
+			return [];
+		}
+	}
+
+	async getUserReciepts({
+		userId,
+		limit,
+		offset,
+	}: { userId: string; limit: number; offset: number }) {
+		const studioReceipts = this.databaseService.db
+			.select({
+				receiptId: studioSessionBookings.id,
+				title: studioSessionBookings.studioName,
+				type: sql<string>`'studio'`,
+				quantity: sql<number>`1`,
+				imageUrl: sql<string>`''`,
+				receiptBarcodeData: studioSessionBookings.recieptBarcodeData,
+			})
+			.from(studioSessionBookings)
+			.where(
+				and(
+					eq(studioSessionBookings.userId, userId),
+					or(
+						eq(studioSessionBookings.status, "completed"),
+						eq(studioSessionBookings.status, "scheduled"),
+						eq(studioSessionBookings.status, "cancelled"),
+					),
+				),
+			);
+
+		const equipmentRentalReceipts = this.databaseService.db
+			.select({
+				receiptId: equipmentRentalBookings.id,
+				title: sql<string>`${equipmentRentals.name}`,
+				type: sql<string>`'equipment'`,
+				quantity: equipmentRentalBookings.quantity,
+				imageUrl: sql<string>`COALESCE(JSON_UNQUOTE(JSON_EXTRACT(${equipmentRentals.imageUrls}, '$[0].url')), '')`,
+				receiptBarcodeData: equipmentRentalBookings.receiptBarcodeData,
+			})
+			.from(equipmentRentalBookings)
+			.leftJoin(
+				equipmentRentals,
+				eq(equipmentRentalBookings.equipmentId, equipmentRentals.id),
+			)
+			.where(
+				and(
+					eq(equipmentRentalBookings.userId, userId),
+					or(
+						eq(equipmentRentalBookings.status, "ongoing"),
+						eq(equipmentRentalBookings.status, "completed"),
+						eq(equipmentRentalBookings.status, "cancelled"),
+					),
+				),
+			);
+
+		const hotelReceipts = this.databaseService.db
+			.select({
+				receiptId: hotelBookings.id,
+				title: sql<string>`${hotels.name}`,
+				type: sql<string>`'hotel'`,
+				quantity: sql<number>`1`,
+				imageUrl: sql<string>`COALESCE(JSON_UNQUOTE(JSON_EXTRACT(${hotelRooms.imageUrls}, '$[0].url')), '')`,
+				receiptBarcodeData: hotelBookings.receiptBarcodeData,
+			})
+			.from(hotelBookings)
+			.leftJoin(hotelRooms, eq(hotelBookings.hotelRoomId, hotelRooms.id))
+			.leftJoin(hotels, eq(hotelRooms.hotelId, hotels.id))
+			.where(
+				and(
+					eq(hotelBookings.userId, userId),
+					or(
+						eq(hotelBookings.status, "completed"),
+						eq(hotelBookings.status, "cancelled"),
+						eq(hotelBookings.status, "confirmed"),
+					),
+				),
+			);
+
+		const foodReceipts = this.databaseService.db
+			.select({
+				receiptId: foodOrders.id,
+				title: sql<string>`${foods.name}`,
+				type: sql<string>`'food'`,
+				quantity: foodOrders.quantity,
+				imageUrl: sql<string>`COALESCE(JSON_UNQUOTE(JSON_EXTRACT(${foods.imageUrls}, '$[0].url')), '')`,
+				receiptBarcodeData: foodOrders.receiptBarcodeData,
+			})
+			.from(foodOrders)
+			.leftJoin(foods, eq(foodOrders.foodId, foods.id))
+			.where(
+				and(
+					eq(foodOrders.userId, userId),
+					or(
+						eq(foodOrders.status, "delivered"),
+						eq(foodOrders.status, "cancelled"),
+					),
+				),
+			);
+
+		const receipts = await unionAll(
+			studioReceipts,
+			equipmentRentalReceipts,
+			hotelReceipts,
+			foodReceipts,
+		)
+			.limit(limit)
+			.offset(offset);
+
+		return receipts;
+	}
+
+	async getUserRecieptDetailsById({
+		receiptId,
+		userId,
+		receiptType,
+	}: {
+		receiptId: string;
+		userId: string;
+		receiptType: "studio" | "equipment" | "hotel" | "food";
+	}) {
+		if (receiptType === "studio") {
+			return await this.databaseService.db
+				.select({
+					studioName: studioSessionBookings.studioName,
+					sessionDate: studioSessionBookings.sessionDate,
+					sessionStartTime: studioSessionBookings.sessionStartTime,
+					sessionEndTime: studioSessionBookings.sessionEndTime,
+					price: studioSessionBookings.totalPrice,
+					receiptBarcodeData: studioSessionBookings.qrcodeData,
+				})
+				.from(studioSessionBookings)
+				.where(
+					and(
+						eq(studioSessionBookings.id, receiptId),
+						eq(studioSessionBookings.userId, userId),
+					),
+				);
+		} else if (receiptType === "equipment") {
+			return await this.databaseService.db
+				.select({
+					equipmentName: equipmentRentals.name,
+					rentalStartDate: equipmentRentalBookings.rentalStartDate,
+					rentalEndDate: equipmentRentalBookings.rentalEndDate,
+					quantity: equipmentRentalBookings.quantity,
+					price: equipmentRentalBookings.totalPrice,
+					receiptBarcodeData: equipmentRentalBookings.receiptBarcodeData,
+				})
+				.from(equipmentRentalBookings)
+				.leftJoin(
+					equipmentRentals,
+					eq(equipmentRentalBookings.equipmentId, equipmentRentals.id),
+				)
+				.where(
+					and(
+						eq(equipmentRentalBookings.id, receiptId),
+						eq(equipmentRentalBookings.userId, userId),
+					),
+				);
+		} else if (receiptType === "hotel") {
+			return await this.databaseService.db
+				.select({
+					hotelName: hotels.name,
+					hotelImageUrl: sql<string>`COALESCE(JSON_UNQUOTE(JSON_EXTRACT(${hotelRooms.imageUrls}, '$[0].url')), '')`,
+					roomName: hotelRooms.name,
+					checkInDate: hotelBookings.checkInDate,
+					checkOutDate: hotelBookings.checkOutDate,
+					price: hotelBookings.totalPrice,
+					location: hotels.state,
+					receiptBarcodeData: hotelBookings.receiptBarcodeData,
+				})
+				.from(hotelBookings)
+				.leftJoin(hotelRooms, eq(hotelBookings.hotelRoomId, hotelRooms.id))
+				.leftJoin(hotels, eq(hotelRooms.hotelId, hotels.id))
+				.where(
+					and(
+						eq(hotelBookings.id, receiptId),
+						eq(hotelBookings.userId, userId),
+					),
+				);
+		} else if (receiptType === "food") {
+			return await this.databaseService.db
+				.select({
+					foodName: foods.name,
+					quantity: foodOrders.quantity,
+					price: foodOrders.totalPrice,
+					addons: foodOrders.foodAddons,
+					receiptBarcodeData: foodOrders.receiptBarcodeData,
+				})
+				.from(foodOrders)
+				.leftJoin(foods, eq(foodOrders.foodId, foods.id))
+				.where(
+					and(eq(foodOrders.id, receiptId), eq(foodOrders.userId, userId)),
+				);
+		} else {
+			return [];
+		}
+	}
+
+	async getUserOrders({
+		userId,
+		offset,
+		limit,
+	}: { userId: string; offset: number; limit: number }) {
+		const fetchedOrders = await this.databaseService.db.query.orders.findMany({
+			where: and(eq(orders.userId, userId), eq(orders.status, "completed")),
+			limit,
+			offset,
+			columns: {
+				status: false,
+				createdAt: false,
+				updatedAt: false,
+			},
+		});
+
+		return fetchedOrders;
 	}
 }
