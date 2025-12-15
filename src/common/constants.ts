52ae057c531f38fca51937ad36281d38b8e3f7c6
Anuolu-2020 <Anuolu1645@gmail.com>
2025-12-15 19:55:23 +0100

    feat: add CLOUDINARY constant

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -3,3 +3,4 @@ export const ID_GENERATOR_LENGTH = 15;
 export const TICKET_ID_LENGTH = 10;
 export const SUCCESSFUL_PAYMENT_WEBHOOK = "successful_payment-webhook";
 export const WEBHOOKS_QUEUE = "webhooks";
+export const CLOUDINARY = "CLOUDINARY";
